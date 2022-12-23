844bc63bb501c152a0d8d3acf243a97b3e262136
andrevale99 <andrefvale@gmail.com>
2022-12-23 11:29:50 -0300

    Classe para manipulacao do tempo do computador

---
+++ hds/Time_Ponto.hpp
@@ -0,0 +1,21 @@
+#ifndef TIME_PONTO_HPP
+#define TIME_PONTO_HPP
+
+#include <chrono>
+#include <ctime>
+#include <iostream>
+
+class Time_Ponto
+{
+
+		public:
+			Time_Ponto();
+			Time_Ponto(int h, int min, int s);
+
+		protected:
+
+		private:
+
+};
+
+#endif
---
+++ src/Time_Ponto.cpp
@@ -0,0 +1,11 @@
+#include "Time_Ponto.hpp"
+
+Time_Ponto::Time_Ponto()
+{
+	std::cout << "TIME_PONTO\n";
+}
+
+Time_Ponto::Time_Ponto(int h, int min, int s)
+{
+	std::cout << "TIME_PONTO\n";
+}
